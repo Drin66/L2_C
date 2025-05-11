200287adef51fee5d3a1954440c382e934931c3b
IZ10na <152287176+IZ10na@users.noreply.github.com>
2025-05-11 23:06:30 +0200

    Add files via upload

--- src/main/java/com/cinema/backend/controllers/UserController.java
+++ src/main/java/com/cinema/backend/controllers/UserController.java
@@ -6,9 +6,11 @@ import com.cinema.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -37,5 +39,10 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(new LoginResponseDTO("Invalid credentials", null, null));
         }
+    }
+    @GetMapping("/api/v1/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
     }
 }
